5806fa1e157d9e9e506daa5961e28ceeaa19853e
Radina Dwiki N <radinal.dwiki.novendra@students.uin-suska.ac.id>
2018-07-06 06:04:34 +0700

    project kp

---
+++ app/src/main/java/org/inkubator/radinaldn/FAQActivity.java
@@ -0,0 +1,15 @@
+package org.inkubator.radinaldn;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Rian on 24/10/2017.
+ */
+public class FAQActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.faq);
+    }
+}
---
+++ app/src/main/java/org/inkubator/radinaldn/Koneksi.java
@@ -0,0 +1,17 @@
+package org.inkubator.radinaldn;
+
+public class Koneksi {
+
+	// Asli dari template
+	public String isi_koneksi()
+	{
+		//String isi = "http://10.0.2.2/ar-lacak/api/"; over emulator
+		String isi = "https://192.168.43.1/ar-lacak/api/"; //over wifi
+		return isi;
+	}
+	// end of asli
+}
+
+
+
+
